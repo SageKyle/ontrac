2e3fb933d830ff79e6724f10f4c61d0321ff3059
SageKyle <ominyipaulo@gmail.com>
2023-01-18 20:01:14 +0100

    fix: change navbar icons to react-icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import { NavLink } from 'react-router-dom';
 
+// icons
+import { BiHomeCircle, BiUserCircle } from 'react-icons/bi';
+import { BsBookmarksFill, BsFillPlusCircleFill } from 'react-icons/bs';
+
 export default function Navbar() {
 	return (
-		<nav className="p-4 min-w-full bg-slate-600 border-t-gray-900 border-2 text-white flex sticky z-50 bottom-0">
+		<nav className="py-2 px-4 min-w-full bg-slate-600 border-t-gray-900 border-2 text-white flex sticky z-50 bottom-0">
 			<ul className="flex justify-evenly align-middle w-[100%]">
 				{/* Home */}
 				<li
@@ -13,20 +17,7 @@ export default function Navbar() {
 						to={'/'}
 						className="flex flex-col items-center justify-center"
 					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="w-8 h-8"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-							/>
-						</svg>
+						<BiHomeCircle className="text-2xl" />
 						<span>Home</span>
 					</NavLink>
 				</li>
@@ -39,20 +30,7 @@ export default function Navbar() {
 						to={'/new'}
 						className="flex flex-col items-center justify-center"
 					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="w-8 h-8"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-							/>
-						</svg>
+						<BsFillPlusCircleFill className="text-2xl" />
 						<span>Add Item</span>
 					</NavLink>
 				</li>
@@ -65,20 +43,7 @@ export default function Navbar() {
 						to={'/'}
 						className="flex flex-col items-center justify-center"
 					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="w-8 h-8"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
-							/>
-						</svg>
+						<BsBookmarksFill className="text-2xl" />
 						<span>Bookmarks</span>
 					</NavLink>
 				</li>
@@ -91,20 +56,7 @@ export default function Navbar() {
 						to={'/sign-in'}
 						className="flex flex-col items-center justify-center"
 					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="w-8 h-8"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-							/>
-						</svg>
+						<BiUserCircle className="text-2xl" />
 						<span>Login</span>
 					</NavLink>
 				</li>
