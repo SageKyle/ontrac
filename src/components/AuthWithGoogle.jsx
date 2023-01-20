7b166bbdab21dc52a97308f27f2142a6f0cbb447
SageKyle <ominyipaulo@gmail.com>
2023-01-20 21:42:03 +0100

    feat: add hook for signing in with Google

--- src/components/AuthWithGoogle.jsx
+++ src/components/AuthWithGoogle.jsx
@@ -1,13 +1,20 @@
 import { FcGoogle } from 'react-icons/fc';
+import useSignInWithGoogle from '../hooks/useSignInWithGoogle';
 
 export default function AuthWithGoogle() {
+	const { signInWithGoogle, error } = useSignInWithGoogle();
+
 	return (
 		<section className="">
 			<div className="flex w-full justify-center item-center my-6">
 				<span className="uppercase text-2xl">or</span>
 			</div>
-
-			<button className="w-full p-4 flex justify-center items-center border-2 border-white bg-white text-slate-600 rounded hover:bg-transparent hover:text-white transition ease-in-out">
+			{error && <p>{error}</p>}
+			<button
+				type="button"
+				onClick={signInWithGoogle}
+				className="w-full p-4 flex justify-center items-center border-2 border-white bg-white text-slate-600 rounded hover:bg-transparent hover:text-white transition ease-in-out"
+			>
 				<FcGoogle className="inline-block mr-2 text-2xl" />
 				<span className="capitalize text-2xl">continue with google</span>
 			</button>
