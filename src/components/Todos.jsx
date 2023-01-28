cbe78ab5964bb7c525a86d80333fd8fd7fd5a1bb
SageKyle <ominyipaulo@gmail.com>
2023-01-28 01:29:54 +0100

    fix: update Todos to show total uncompleted todos
    also use date-fns to format todo date

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,38 +1,51 @@
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import { toast } from 'react-toastify';
 import useFetchDoc from '../hooks/db/useFetchDoc';
 import Loading from '../utils/Loading';
 
 export default function Todos() {
 	const { docs: todos, isPending, error } = useFetchDoc('todos');
+	const uncompletedTodos = todos
+		? todos.filter((todo) => todo.completed === false).length
+		: 0;
 
 	return (
-		<section className="mb-4 flex items-start justify-start flex-wrap">
-			{isPending && <Loading />}
-			{error && toast.error(error)}
-			{todos &&
-				todos.map((todo) => (
-					<div
-						key={Math.random()}
-						className="m-2 p-2 border-2 relative cursor-pointer rounded sm:w-full md:w-[20rem]"
-					>
-						<h1
-							className={
-								todo.completed
-									? 'capitalize line-through text-2xl'
-									: 'capitalize text-2xl'
-							}
+		<>
+			<h2 className="text-2xl my-4">
+				You have {uncompletedTodos} uncompleted tasks
+			</h2>
+			<section className="mb-4 flex items-start justify-start flex-wrap">
+				{isPending && <Loading />}
+				{error && toast.error(error)}
+				{todos &&
+					todos.map((todo) => (
+						<div
+							key={Math.random()}
+							className="m-2 p-2 border-2 relative cursor-pointer rounded sm:w-full md:w-[20rem]"
 						>
-							{todo.todo}
-						</h1>
-						{todo.completed && (
-							<span className="absolute top-0 right-1 block text-[#fad6a5] font-bold">
-								Done
-							</span>
-						)}
-						<p>{todo.note}</p>
-						<p>{todo.createdAt.toDate().toDateString()}</p>
-					</div>
-				))}
-		</section>
+							<h4
+								className={
+									todo.completed
+										? 'capitalize line-through text-2xl'
+										: 'capitalize text-2xl text-[#fad6a5]'
+								}
+							>
+								{todo.todo}
+							</h4>
+							{todo.completed && (
+								<span className="absolute top-0 right-1 block text-[#fad6a5] font-bold">
+									Done
+								</span>
+							)}
+							<p className="text-sm">{todo.note}</p>
+							<p>
+								{formatDistanceToNow(todo.createdAt.toDate(), {
+									addSuffix: true,
+								})}
+							</p>
+						</div>
+					))}
+			</section>
+		</>
 	);
 }
