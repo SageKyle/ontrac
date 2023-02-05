740ed05d3985dcb5004734c29510cbb1904542d8
SageKyle <ominyipaulo@gmail.com>
2023-02-05 16:49:34 +0100

    feat: install Tenstack Query for managing fetch requests

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'regenerator-runtime/runtime';
@@ -5,10 +7,15 @@ import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<AuthContextProvider>
-			<App />
-		</AuthContextProvider>
+		<QueryClientProvider client={queryClient}>
+			<AuthContextProvider>
+				<App />
+			</AuthContextProvider>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	</React.StrictMode>
 );
