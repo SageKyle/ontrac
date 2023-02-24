53691e9b9e6546ea609c41c04d0a5ab34998a553
SageKyle <ominyipaulo@gmail.com>
2023-02-24 18:23:22 +0100

    feat: render only starred tasks initially
    add a 'more' button which will show all tasks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,41 @@
+import { useRef } from 'react';
 import AllNotes from './notes/AllNotes';
 import AllTasks from './tasks/AllTasks';
+import StarredTasks from './tasks/StarredTasks';
 
 export default function Home() {
+	const tasksRef = useRef();
+	const toggleRef = useRef();
+
+	function showMore() {
+		tasksRef.current.classList.remove('hidden');
+		tasksRef.current.classList.add('hidden');
+	}
+
 	return (
 		<section>
 			<h3 className="capitalize my-4 text-2xl text-[#fad6a5] font-bold">
 				Tasks
 			</h3>
-			<AllTasks />
+			<>
+				{/* Starred Tasks */}
+				<div className="mb-4 border-b-2 border-[#fad6a5] w-full">
+					<StarredTasks />
+				</div>
+
+				<h4
+					className="text-[#fad6a5] m-2 hover:underline"
+					ref={toggleRef}
+					onClick={showMore}
+				>
+					More
+				</h4>
+				{/* All Tasks */}
+				<div className="hidden w-full" ref={tasksRef}>
+					<AllTasks />
+				</div>
+			</>
+			{/* Notes */}
 			<h3 className="capitalize my-4 text-2xl text-[#fad6a5] font-bold">
 				notes
 			</h3>
