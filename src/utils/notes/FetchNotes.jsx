438e51aa5fdd5e1fa907929ce4d4c2e07f30955b
SageKyle <ominyipaulo@gmail.com>
2023-02-26 19:51:19 +0100

    feat: replace TanStack with useEffect
    used the effect hook to fetch notes instead of using TanStack
    
    BREAKING CHANGES
    This is yet to undergo proper tests, there's no guarantee if it works

--- src/utils/notes/FetchNotes.jsx
+++ src/utils/notes/FetchNotes.jsx
@@ -1,15 +1,43 @@
-import { useQuery } from '@tanstack/react-query';
+// import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import { fetchAllNotes } from '../../hooks/db/useFetchDoc';
 
 export default function FetchNotes() {
-	const queryDocs = useQuery({
-		queryKey: ['notes'],
-		queryFn: fetchAllNotes,
-	});
+	const [allNotes, setAllNotes] = useState(null);
+	const [error, setError] = useState(null);
+	const [isPending, setIsPending] = useState(false);
+	const [isCancelled, setIsCancelled] = useState(false);
 
-	const allNotes = queryDocs.data,
-		isPending = queryDocs.isFetching,
-		error = queryDocs.error;
+	useEffect(() => {
+		setIsPending(true);
+		setError(null);
+
+		if (!isCancelled) {
+			try {
+				const doc = fetchAllNotes();
+				setAllNotes(doc);
+				setIsPending(false);
+				setError(null);
+			} catch (err) {
+				console.error(err);
+				setIsPending(false);
+				setError(err);
+			}
+		}
+
+		return () => {
+			setIsCancelled(true);
+		};
+	}, []);
+
+	// const queryDocs = useQuery({
+	// 	queryKey: ['notes'],
+	// 	queryFn: fetchAllNotes,
+	// });
+
+	// const allNotes = queryDocs.data,
+	// 	isPending = queryDocs.isFetching,
+	// 	error = queryDocs.error;
 
 	const starredNotes = allNotes
 		? allNotes.filter((note) => note.starred === true)
