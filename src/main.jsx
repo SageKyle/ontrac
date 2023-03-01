3f660004cb8f82b664c813d47529a7efc690f313
SageKyle <ominyipaulo@gmail.com>
2023-03-01 20:00:11 +0100

    chore: uninstall TanStack query/devtools dependency

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'regenerator-runtime/runtime';
@@ -7,15 +5,10 @@ import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 import './index.css';
 
-// const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		{/* <QueryClientProvider client={queryClient}> */}
 		<AuthContextProvider>
 			<App />
 		</AuthContextProvider>
-		{/* <ReactQueryDevtools initialIsOpen={false} /> */}
-		{/* </QueryClientProvider> */}
 	</React.StrictMode>
 );
