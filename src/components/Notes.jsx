a0e4ecca48a666def2d5837cdb1c28f347968691
SageKyle <ominyipaulo@gmail.com>
2023-05-26 15:31:03 +0100

    disable interaction with notes if modal is open
    if the delete modal is shown,
    the user shouldn't be able to interact with the entire note component.

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -19,44 +19,50 @@ export default function Notes({ notes, isPending, error }) {
 	}
 
 	return (
-		<section className="mb-4 flex items-start justify-start flex-wrap">
-			{isPending && <Loading />}
-			{!notes && <p>There's nothing here!</p>}
-			{error && (
-				<ErrorModal>
-					<p>{error}</p>
-				</ErrorModal>
-			)}
-			{notes &&
-				notes.map((note) => (
-					<article
-						key={note.id}
-						className="m-2 p-2 flex flex-col border-2 relative cursor-pointer rounded sm:w-full md:w-[20rem] max-h-[20rem] overflow-y-auto"
-					>
-						{note.title && (
-							<h1 className="mb-2 border-b-2 border-slate-500 capitalize font-bold">
-								{note.title}
-							</h1>
-						)}
-
-						<Link to={'edit-note/' + note.id}>
-							<pre className="max-w-full mb-4">{note.note}</pre>
-						</Link>
-						<p className="text-xs mt-6 absolute bottom-2">
-							Edited{' '}
-							{formatDistanceToNow(note.createdAt.toDate(), {
-								addSuffix: true,
-							})}
-						</p>
-						<button
-							disabled={modalProp.open}
-							onClick={() => setmodalProp({ id: note.id, open: true })}
-							className="sticky block ml-auto right-4 bottom-0 z-10 bg-[#567189] rounded-full p-2"
+		<>
+			{/* disable this section if modal is open */}
+			<section
+				className={`${
+					modalProp.open ? 'pointer-events-none opacity-40' : ''
+				} mb-4 flex items-start justify-start flex-wrap`}
+			>
+				{isPending && <Loading />}
+				{!notes && <p>There's nothing here!</p>}
+				{error && (
+					<ErrorModal>
+						<p>{error}</p>
+					</ErrorModal>
+				)}
+				{notes &&
+					notes.map((note) => (
+						<article
+							key={note.id}
+							className="m-2 p-2 flex flex-col border-2 relative cursor-pointer rounded sm:w-full md:w-[20rem] max-h-[20rem] overflow-y-auto"
 						>
-							<FaTrash />
-						</button>
-					</article>
-				))}
+							{note.title && (
+								<h1 className="mb-2 border-b-2 border-slate-500 capitalize font-bold">
+									{note.title}
+								</h1>
+							)}
+
+							<Link to={'edit-note/' + note.id}>
+								<pre className="max-w-full mb-4">{note.note}</pre>
+							</Link>
+							<p className="text-xs mt-6 absolute bottom-2">
+								Edited{' '}
+								{formatDistanceToNow(note.createdAt.toDate(), {
+									addSuffix: true,
+								})}
+							</p>
+							<button
+								onClick={() => setmodalProp({ id: note.id, open: true })}
+								className="sticky block ml-auto right-4 bottom-0 z-10 bg-[#567189] rounded-full p-2"
+							>
+								<FaTrash />
+							</button>
+						</article>
+					))}
+			</section>
 			{/* note delete modal */}
 			{modalProp.open && (
 				<DeleteConfirmationModal
@@ -64,6 +70,6 @@ export default function Notes({ notes, isPending, error }) {
 					onClose={() => setmodalProp({ ...modalProp, open: false })}
 				/>
 			)}
-		</section>
+		</>
 	);
 }
