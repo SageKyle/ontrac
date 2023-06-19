7ed55f42c72c82abfb47ca1006385b64522280f0
SageKyle <ominyipaulo@gmail.com>
2023-06-19 01:06:13 +0100

    feat: update note styles
    BREAKING CHANGES
    all starred notes now have a star icon by the top right
    wrap notes with a grid container

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -6,6 +6,7 @@ import DeleteConfirmationModal from '../utils/modals/DeleteConfirmationModal';
 import ErrorModal from '../utils/modals/ErrorModal';
 
 import { useState } from 'react';
+import { BsStarFill } from 'react-icons/bs';
 import { FaTrash } from 'react-icons/fa';
 
 export default function Notes({ notes, isPending, error }) {
@@ -24,7 +25,7 @@ export default function Notes({ notes, isPending, error }) {
 			<section
 				className={`${
 					modalProp.open ? 'pointer-events-none opacity-40' : ''
-				} mb-4 flex items-start justify-start flex-wrap`}
+				} mb-4 w-full flex items-start justify-start`}
 			>
 				{isPending && <Loading />}
 
@@ -34,35 +35,46 @@ export default function Notes({ notes, isPending, error }) {
 						<p>{error}</p>
 					</ErrorModal>
 				)}
-				{notes &&
-					notes.map((note) => (
-						<article
-							key={note.id}
-							className="m-2 p-2 flex flex-col border-2 relative cursor-pointer rounded sm:w-full md:w-[20rem] max-h-[20rem] overflow-y-auto"
-						>
-							{note.title && (
-								<h1 className="mb-2 border-b-2 border-slate-500 capitalize font-bold">
-									{note.title}
-								</h1>
-							)}
-
-							<Link to={'edit-note/' + note.id}>
-								<pre className="max-w-full mb-4">{note.note}</pre>
-							</Link>
-							<p className="text-xs mt-6 absolute bottom-2">
-								Edited{' '}
-								{formatDistanceToNow(note.createdAt.toDate(), {
-									addSuffix: true,
-								})}
-							</p>
-							<button
-								onClick={() => setmodalProp({ id: note.id, open: true })}
-								className="sticky block ml-auto right-4 bottom-0 z-10 bg-[#567189] rounded-full p-2"
+				<div className="w-full grid items-start justify-center grid-flow-row-dense grid-cols-1 md:grid-cols-2 gap-8  lg:grid-cols-3 relative">
+					{notes &&
+						notes.map((note) => (
+							<article
+								key={note.id}
+								className="p-2 flex flex-col justify-between border-2 relative cursor-pointer rounded w-full h-[20rem] overflow-y-auto"
 							>
-								<FaTrash />
-							</button>
-						</article>
-					))}
+								<div
+									aria-label="starred note"
+									className="absolute top-2 right-2 text-2xl"
+								>
+									{note.starred && <BsStarFill className="text-[#fad6a5]" />}
+								</div>
+								{note.title && (
+									<h1 className="mb-2 border-b-2 border-slate-500 capitalize font-bold">
+										{note.title}
+									</h1>
+								)}
+
+								<Link
+									to={'edit-note/' + note.id}
+									className="max-w-full mb-4 justify-self-start"
+								>
+									<pre>{note.note}</pre>
+								</Link>
+								<small className="text-xs py-2 mt-auto sticky justify-self-end bottom-0 text-cyan-200 bg-[#252c53]">
+									Edited{' '}
+									{formatDistanceToNow(note.createdAt.toDate(), {
+										addSuffix: true,
+									})}
+								</small>
+								<button
+									onClick={() => setmodalProp({ id: note.id, open: true })}
+									className="sticky w-auto inline-block ml-auto right-4 bottom-2 z-10 bg-[#567189] rounded-full p-2"
+								>
+									<FaTrash aria-label="delete note" />
+								</button>
+							</article>
+						))}
+				</div>
 			</section>
 			{/* note delete modal */}
 			{modalProp.open && (
