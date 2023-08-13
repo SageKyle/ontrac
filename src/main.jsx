acfb40616971593e2c8705b051c373cc03014ff7
SageKyle <ominyipaulo@gmail.com>
2023-08-13 15:48:46 +0100

    fix: switch pwa to vite-plugin-pwa
    - replace previous service worker config with a new one that uses `vite-plugin-pwa`
    - move `manifest.json` content into `vite.config.js`

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import 'regenerator-runtime/runtime'
 import App from './App'
 import { AuthContextProvider } from './context/AuthContext'
 import './index.css'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
@@ -13,5 +12,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		</AuthContextProvider>
 	</React.StrictMode>
 )
-
-serviceWorkerRegistration.register()
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,50 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-	plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: 'auto',
+			devOptions: { enabled: true },
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+			},
+			manifest: {
+				short_name: 'OnTrac',
+				name: 'OnTrac Personal Manager',
+				version: '1.1.0',
+				icons: [
+					{
+						src: 'favicon.ico',
+						sizes: '64x64 32x32 24x24 16x16',
+						type: 'image/x-icon',
+					},
+					{
+						src: 'android-chrome-192x192.png',
+						type: 'image/png',
+						sizes: '192x192',
+					},
+					{
+						src: 'android-chrome-512x512.png',
+						type: 'image/png',
+						sizes: '512x512',
+						purpose: 'any',
+					},
+					{
+						src: 'android-chrome-512x512.png',
+						type: 'image/png',
+						sizes: '512x512',
+						purpose: 'maskable',
+					},
+				],
+				start_url: '.',
+				display: 'standalone',
+				theme_color: '#252c53',
+				background_color: '#ffffff',
+			},
+		}),
+	],
 })
 
 // https://vitejs.dev/config/
